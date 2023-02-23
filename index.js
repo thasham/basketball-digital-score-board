c63c851982ca503fc5b1bd9369e73cde8d48c836
thasham <113823706+thasham@users.noreply.github.com>
2023-02-23 14:07:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+let scoreEl = document.getElementById("score-el")
+let score2El = document.getElementById("score2-el")
+let count = 0
+function one(){
+    count += 1
+    scoreEl.textContent = count    
+}
+
+function two(){
+    count += 2
+    scoreEl.textContent = count
+    
+}
+
+function three(){
+    count += 3
+    scoreEl.textContent = count
+    
+}
+let count2 = 0
+function one2(){
+    count2 += 1
+    score2El.textContent = count2    
+}
+
+function two2(){
+    count2 += 2
+    score2El.textContent = count2
+    
+}
+
+function three2(){
+    count2 += 3
+    score2El.textContent = count2
+    
+}
+
+
+
